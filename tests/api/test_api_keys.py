315fc180c4a63761bc0e045cdef2154893ba4329
Mark Burnett <mark.m.burnett@gmail.com>
2014-08-28 15:22:07 -0500

    extract post_api_key method

--- tests/api/test_api_keys.py
+++ tests/api/test_api_keys.py
@@ -5,52 +5,44 @@ import unittest
 
 
 class PostApiKeyList(BaseFlaskTest):
-    def test_should_return_401_with_invalid_user(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('baduser', 'badpass'),
+    def post_api_key(self, username, password):
+        return self.client.post('/v1/api-keys', headers={
+            'Authorization': self.basic_auth_header(username, password),
         })
+
+    def test_should_return_401_with_invalid_user(self):
+        response = self.post_api_key('baduser', 'badpass')
 
         self.assertEqual(response.status_code, 401)
 
     def test_should_set_www_authenticate_header_with_invalid_user(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('baduser', 'badpass'),
-        })
+        response = self.post_api_key('baduser', 'badpass')
 
         self.assertEqual(response.headers['WWW-Authenticate'], 'API-Key')
 
     def test_should_return_401_with_invalid_password(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('alice', 'badpass'),
-        })
+        response = self.post_api_key('alice', 'badpass')
 
         self.assertEqual(response.status_code, 401)
 
     def test_should_set_www_authenticate_header_with_invalid_password(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('alice', 'badpass'),
-        })
+        response = self.post_api_key('alice', 'badpass')
 
         self.assertEqual(response.headers['WWW-Authenticate'], 'API-Key')
 
     def test_should_return_201(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('alice', 'apass'),
-        })
+        response = self.post_api_key('alice', 'apass')
         self.assertEqual(response.status_code, 201)
 
     def test_should_set_location_header(self):
-        response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('alice', 'apass'),
-        })
+        response = self.post_api_key('alice', 'apass')
         self.assertTrue(re.search(
             r'^http://localhost[:\d+]?/v1/api-keys/\w+:k$',
             response.headers['Location']))
 
     def test_should_return_valid_api_key(self):
-        post_response = self.client.post('/v1/api-keys', headers={
-            'Authorization': self.basic_auth_header('alice', 'apass'),
-        })
+        post_response = self.post_api_key('alice', 'apass')
+
         data = json.loads(post_response.data)
         self.assertIn('api-key', data)
         api_key = data['api-key']
